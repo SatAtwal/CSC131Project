2aafa39d2bc9f254e34af9308b01ee363cafe126
Tomas <Tomas@10.113.37.196>
2019-04-23 23:46:07 -0700

    Added found() method to Owner

--- Lost_Item_Locator/src/Locator/Owner.java
+++ Lost_Item_Locator/src/Locator/Owner.java
@@ -1,5 +1,17 @@
 package Locator;
 
+/** 
+ * 
+ * This class simulates the behaviors of the owner of the lost
+ * device.  It initiates the application events and signals termination
+ * after the object is found.
+ * 
+ * @author Tomas Albor
+ * 
+ */
+
+
+
 public class Owner
 {
 	private boolean itemLost;
@@ -12,13 +24,29 @@ public class Owner
 		this.itemFound = false;
 	}
 	
-	public void flagLost (itemLost)
+	public void flagLost (boolean itemLost)
 	{
 		this.itemLost=true;
 	}
 	
-	public boolean getLost (itemLost)
+	public boolean getLost (boolean itemLost)
 	{
-		return this.itemLost
+		return this.itemLost;
+	}
+	
+	public void found ()
+	{
+		System.out.println("\nMy object was found!"
+				+ " I just received a notification with"
+				+ "its coordinates!");
+		
+		this.itemLost = false;
+		this.itemFound = true;
+		
+		System.out.println("\nitemLost="+this.itemLost
+							+ "\nitemFound="+this.itemFound);
+		
+		
+	
 	}
 }
